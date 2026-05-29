54f8fc331aedfb22e3a6444d95d2caba566ad550
ardhikaxx <ardhikayanuar58@gmail.com>
2026-05-30 06:45:54 +0700

    chore: menonaktifkan pembuatan dummy customer pada UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -35,24 +35,24 @@ class UserSeeder extends Seeder
             ]
         );
 
-        // Dummy Customers (Users with pelanggan role)
-        $customers = [
-            ['name' => 'Budi Santoso', 'email' => 'budi@gmail.com', 'phone' => '081234567890'],
-            ['name' => 'Siti Aminah', 'email' => 'siti@gmail.com', 'phone' => '082234567891'],
-            ['name' => 'Andi Wijaya', 'email' => 'andi@gmail.com', 'phone' => '083334567892'],
-        ];
+        // // Dummy Customers (Users with pelanggan role)
+        // $customers = [
+        //     ['name' => 'Budi Santoso', 'email' => 'budi@gmail.com', 'phone' => '081234567890'],
+        //     ['name' => 'Siti Aminah', 'email' => 'siti@gmail.com', 'phone' => '082234567891'],
+        //     ['name' => 'Andi Wijaya', 'email' => 'andi@gmail.com', 'phone' => '083334567892'],
+        // ];
 
-        foreach ($customers as $c) {
-            User::updateOrCreate(
-                ['email' => $c['email']],
-                [
-                    'role_id'  => $pelanggan->id,
-                    'name'     => $c['name'],
-                    'phone'    => $c['phone'],
-                    'password' => Hash::make('password'),
-                    'is_active' => true,
-                ]
-            );
-        }
+        // foreach ($customers as $c) {
+        //     User::updateOrCreate(
+        //         ['email' => $c['email']],
+        //         [
+        //             'role_id'  => $pelanggan->id,
+        //             'name'     => $c['name'],
+        //             'phone'    => $c['phone'],
+        //             'password' => Hash::make('password'),
+        //             'is_active' => true,
+        //         ]
+        //     );
+        // }
     }
 }
